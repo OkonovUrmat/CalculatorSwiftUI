caec6d5d504f0c7afdeec9818b0e6fcbd8cb793d
OkonovUrmat <okonov.okonov@gmail.com>
2020-06-28 09:51:02 +0600

    ContentView: The size of container for each buttons

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 ForEach(["7", "8", "9", "X"], id: \.self) { button in
                     Text(button)
                         .font(.system(size: 32))
+                    .frame(width: 80, height: 80)
                 }
             }
         }
